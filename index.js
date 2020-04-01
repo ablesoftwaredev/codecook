f1eaf0bfac6c8e2e23d5dfe052acb00abc2a1714
AbleSoftwareDev <ablesoftwaredev@gmail.com>
2020-04-02 00:35:05 +0200

    can inject and format single line code snippets in .py files perfectly
    next  try for .js file with nested structures

--- index.js
+++ index.js
@@ -13,7 +13,6 @@
 const fs = require("fs")
 const path = require("path")
 
-
 const insert = function(codeData) {
     // split code string into array via \n
     let codeLines = codeData.code.split("\n")
@@ -52,46 +51,95 @@ const insert = function(codeData) {
 }
 
 /**
- * insert code in specified file using search expression
+ * loops over the payloadArray and injects code snippets at specified patterns
  * @param {string} targetFilePath Path to codefile to be modified
- * @param {string} searchPattern  Search pattern in codefile
- * @param {string} codeToInject   code snippet to be injected
-**/
-const inject = function(targetFilePath, searchPattern, codeToInject) {
+ * @param {string[]} payloadArray  objects array with search patterns & code injections
+ **/
+const inject = function(targetFilePath, payloadArray) {
     // open target file
     fs.readFile(targetFilePath, (err, data) => {
         if (err) {
             console.log(err)
         } else {
-            // read its code in an array
+            // read codefile's code in an array
             let code = data.toString()
-            let codeArray = code.split('\n')
+            let codeArray = code.split("\n")
+
+            // the index where search pattern was found
             let insertIndex = -1
-            // loop through each line in array and check if search pattern is found
-            codeArray.forEach((line) => {
-                if(line.includes(searchPattern))
+
+            // string representation of tabs to insert
+            let tabString = ""
+            let tabsCount = 0
+
+            // code to be injected with tabs prefixed
+            // this addes code with proper indentation
+            let tabbedInjection = ""
+
+            // console.log(codeArray)
+            // console.log(`insertIndex: ${insertIndex}`)
+
+            // loop through the payloadArray and perform all injections
+            payloadArray.forEach(payload => {
+                // loop through each line in codeArray and check if search pattern is found
+                codeArray.forEach(line => {
+                    if (line.includes(payload.searchPattern)) {
+                        // insertIndex stores the index for adding code injection
+                        insertIndex = codeArray.indexOf(line) + 1 // TODO: add if / else for inject above or below search pattern
+                        // find number of tabs before search pattern line
+                        // this will be the base tabs count for code injection lines
+                        // console.log(`line:${line}`)
+                        for (let index = 0; index < line.length; index++) {
+                            if (line.charCodeAt(index) == 32) {
+                                // console.log(line.charCodeAt(index))
+                                tabsCount += 1
+                                tabString += " "
+                            } else {
+                                break
+                            }
+                        }
+                        // console.log(`line:${line}`)
+                        // console.log(`tabsCount: ${tabsCount}`)
+                        // console.log(`tabString.length: ${tabString.length}`)
+                        // console.log(`tabString:${tabString}abc`)
+
+                        // prefix tabs with tabbedInjection
+                        // for (const codeLine of payload.injection) {
+                        //     tabbedInjection += (tabString + codeLine)
+                        // }
+                        tabbedInjection += (`${tabString}${payload.injection}`)
+                        // console.log(`tabbedInjection:${tabbedInjection}`)
+                    }
+                })
+                // if search pattern is found, perform code injection
+                if(insertIndex !== -1)
                 {
-                    insertIndex = codeArray.indexOf(line)
+                    // splice adds code at specified index
+                    // 0 means no elements are deleted from array
+                    codeArray.splice(insertIndex, 0, tabbedInjection)
+
+                    // reset values for next payload iteration
+                    insertIndex = -1
+                    tabString = ''
+                    tabsCount = 0
+                    tabbedInjection = ''
                 }
             })
-            // if found, insert code above / below the line index
-            if(insertIndex !== -1)
-            {
-                codeArray.splice(insertIndex+1, 0, codeToInject)
-            } else {
-                console.log('search pattern not found')
-            }
+            // console.log(`codeArray: ${codeArray}`)
+
             // make a string from the array
+            let codeString = ""
             codeString = codeArray.join('\n')
             // fs.writeFile the string in target file
             fs.writeFile(targetFilePath, codeString, (error) => {
                 if (error === null)
                 {
-                    console.log('code injected successfully')
+                    console.log('code injections successfull')
                 } else {
                     console.log(err)
                 }
             })
+            codeString = ''
         }
     })
 }
