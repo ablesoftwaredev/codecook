86b6f7d3779b198d4627b763aa02fc95a2a82a99
AbleSoftwareDev <ablesoftwaredev@gmail.com>
2020-04-02 21:18:34 +0200

    can inject code above or below
    + removed insert() function

--- index.js
+++ index.js
@@ -1,54 +1,10 @@
 /**
  * Responsibility:
- * searches for pattern in specified code file and inserts specified code above/below it.
+ * searches for pattern in specified code file and inserts specified code above / below it.
  * it then returns the modified code
- * params for codeData:
- * "rootPath": string
-   "code": string
-   "searchPattern": string
-   "cmdName": string
-   "insertLevel": 1 = below, -1 = above,
-   "codeToInsert": string
  */
 const fs = require("fs")
 const path = require("path")
-
-const insert = function(codeData) {
-    // split code string into array via \n
-    let codeLines = codeData.code.split("\n")
-
-    // find the index of searchPattern in codeLines
-    let searchPattern = codeData["searchPattern"]
-    let insertIndex = 0
-    codeLines.forEach(line => {
-        if (line.includes(searchPattern)) {
-            insertIndex = codeLines.indexOf(line)
-        }
-    })
-    // if pattern found, insert data above / below it
-    if (insertIndex != 0) {
-        codeLines.splice(insertIndex + 1, 0, codeData["codeToInsert"])
-    }
-    // write updated code to extension.js
-    // create code string
-    let codeString = ""
-    codeLines.forEach(line => {
-        codeString += line += "\n"
-    })
-    let testPath = path.join(codeData.rootPath, "extension.js")
-    // write updated code to extension.js
-    fs.writeFile(testPath, codeString, error => {
-        if (error === null) {
-            vscode.window.showInformationMessage(
-                "command created successfully."
-            )
-        } else {
-            vscode.window.showInformationMessage("command not created...")
-        }
-    })
-
-    return codeString
-}
 
 /**
  * loops over the payloadArray and injects code snippets at specified patterns
@@ -81,7 +37,13 @@ const inject = function(targetFilePath, payloadArray) {
                 codeArray.forEach(line => {
                     if (line.includes(payload.searchPattern)) {
                         // insertIndex stores the index for adding code injection
-                        insertIndex = codeArray.indexOf(line) + 1 // TODO: add if / else for inject above or below search pattern
+                        if(payload.injectAbove){
+                            // inject code snippet above search pattern code line
+                            insertIndex = codeArray.indexOf(line)
+                        } else {
+                            // inject code snippet below search pattern code line
+                            insertIndex = codeArray.indexOf(line) + 1
+                        }
                         // find number of tabs before search pattern line
                         // this will be the base tabs count for code injection lines
                         for (let index = 0; index < line.length; index++) {
@@ -131,4 +93,4 @@ const inject = function(targetFilePath, payloadArray) {
     })
 }
 
-module.exports = { insert, inject }
+module.exports = { inject }
